6a17746031c4af01c32e3cfc946c69795bb3d7f7
deepakporwal <er.deepakporwal@gmail.com>
2021-10-05 11:44:52 +0530

    anonymous type example

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,7 +14,9 @@ namespace ConsoleApp1
            
             Console.WriteLine("Hello World! God is great !");
 
-
+            // create a anynymous type 
+            var data = new { Id = 1, Name = "Deepak Porwal", IsActice = true };
+            Console.WriteLine("the value of Id {0} and Name is {1}",data.Id,data.Name);
 
             Console.ReadLine();
         }
