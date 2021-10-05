5c91b5637041a8d84f7c96651467ebc94139cef7
deepakporwal <er.deepakporwal@gmail.com>
2021-10-05 11:29:11 +0530

    partial class example added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using ConsoleApp1.interfaces;
+using System;
 
 namespace ConsoleApp1
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ICustomer obj = new Customer();
+            obj.Add();
+            obj.GetAllCustomers();
+           
+            Console.WriteLine("Hello World! God is great !");
+
+
+
             Console.ReadLine();
         }
     }
---
+++ ConsoleApp1/implementations/Customer1.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public partial class Customer : ICustomer
+    {
+        public void Add() 
+        {
+            Console.WriteLine("customer added.");
+        }
+    }
+}
