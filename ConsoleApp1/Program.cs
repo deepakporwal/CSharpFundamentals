1ba2f8369bb0efa1c5c40610f79059cd6ab6f7d2
deepakporwal <er.deepakporwal@gmail.com>
2021-10-05 12:59:33 +0530

    function overloading

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,6 +18,10 @@ namespace ConsoleApp1
             var data = new { Id = 1, Name = "Deepak Porwal", IsActice = true };
             Console.WriteLine("the value of Id {0} and Name is {1}",data.Id,data.Name);
 
+            SimpleCalss simplobj = new SimpleCalss();
+            Console.WriteLine(simplobj.Sum(1.23, 2.34));
+            Console.WriteLine(simplobj.Sum(1, 2));
+            Console.WriteLine(simplobj.Sum(1, 2,3));
             Console.ReadLine();
         }
     }
---
+++ ConsoleApp1/SimpleCalss.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class SimpleCalss
+    {
+        public int Sum(int x,int y) 
+        {
+            return x + y;
+        }
+
+        public int Sum(int x, int y,int z)
+        {
+            return x + y + z;
+        }
+
+        public double Sum(double x, double y) 
+        {
+            return x + y;
+        }
+    }
+}
